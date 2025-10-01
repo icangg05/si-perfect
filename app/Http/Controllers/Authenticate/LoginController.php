19a47495bc7fcdd296aaf0b1e6ec127296bdc8dd
icangg05 <ilmifaizan2002@gmail.com>
2025-10-02 00:29:51 +0800

    up

--- app/Http/Controllers/Authenticate/LoginController.php
+++ app/Http/Controllers/Authenticate/LoginController.php
@@ -49,6 +49,6 @@ class LoginController extends Controller
     Auth::logout();
     $request->session()->invalidate();
     $request->session()->regenerateToken();
-    return redirect()->route('login');
+    return redirect()->route('beranda');
   }
 }
